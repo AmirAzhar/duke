ae4e733fb4e758769308a81cd8e117716a2140b3
Amir Azhar <e0309641@u.nus.edu>
2019-08-23 21:41:16 +0800

    Level 3 Complete

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,15 +8,23 @@ public class Duke {
 
         Scanner scanner = new Scanner(System.in);
         String myString = scanner.nextLine();
-        List texts = new ArrayList();
+        List<Task> texts = new ArrayList();
+
         while(!myString.equals("bye")){
+            String[] splitter = myString.split(" ");
             if (myString.equals("list")){
                 for (int i=0; i < texts.size(); i++)
-                    System.out.println((i+1) + ". " + texts.get(i));
+                    System.out.println((i+1) + ". " + "[" + texts.get(i).getStatusIcon() + "] " + texts.get(i).description);
+            }
+            else if (splitter[0].equals("done")){
+                int taskNumber = Integer.parseInt(splitter[1]);
+                texts.get(taskNumber - 1).setDone();
             }
             else {
+                Task task = new Task(myString);
+                texts.add(task);
                 System.out.println("added: " + myString);
-                texts.add(myString);
+
             }
                 myString = scanner.nextLine();
         }
