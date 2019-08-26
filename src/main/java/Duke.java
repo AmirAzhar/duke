337152b1d90e26e0156efa990733a82468700c39
Amir Azhar <e0309641@u.nus.edu>
2019-08-26 10:58:47 +0800

    Level 5 Complete

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,47 +15,64 @@ public class Duke {
 
             String[] splitter1 = myString.split(" ");
 
-            //command is list
-            if (myString.equals("list")){
-                System.out.println("Here are the tasks in your list:");
-                for (int i=0; i < texts.size(); i++)
-                    System.out.println((i+1) + ". " + texts.get(i).toString());
+            try {
+                //commands have no description
+                if (myString.equals("todo") || myString.equals("event") || myString.equals("deadline")){
+                    throw new DukeExceptions("☹ OOPS!!! The description of a " + myString + " cannot be empty.");
+                }
+
+                //command is list
+                else if (myString.equals("list")) {
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < texts.size(); i++)
+                        System.out.println((i + 1) + ". " + texts.get(i).toString());
+                }
+
+                //command is done
+                else if (splitter1[0].equals("done")) {
+                    int taskNumber = Integer.parseInt(splitter1[1]);
+                    texts.get(taskNumber - 1).setDone();
+                }
+
+                //command is deadline
+                else if (splitter1[0].equals("deadline")) {
+                    String D = myString.substring(9); // after 9 letters
+                    String[] splitterD = myString.split("/by ");
+                    Deadline deadline = new Deadline(splitterD[0], splitterD[1]);
+                    texts.add(deadline);
+                    System.out.println("Got it. I've added this task:\n  " + texts.get(texts.size() - 1).toString());
+                    System.out.println("Now you have " + texts.size() + " task(s) in the list.");
+                }
+
+                //command is event
+                else if (splitter1[0].equals("event")) {
+                    String E = myString.substring(9); // after 6 letters
+                    String[] splitterE = myString.split("/at ");
+                    Event event = new Event(splitterE[0], splitterE[1]);
+                    texts.add(event);
+                    System.out.println("Got it. I've added this task:\n  " + texts.get(texts.size() - 1).toString());
+                    System.out.println("Now you have " + texts.size() + " task(s) in the list.");
+                }
+
+                //command is todos
+                else if (splitter1[0].equals("todo")) {
+                    String tempT = myString.substring(5);
+                    ToDos todo = new ToDos(tempT);
+                    texts.add(todo);
+                    System.out.println("Got it. I've added this task:\n  " + texts.get(texts.size() - 1).toString());
+                    System.out.println("Now you have " + texts.size() + " task(s) in the list.");
+                }
+
+                //random commands that isn't any of the above
+                else{
+                    throw new DukeExceptions("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                }
             }
 
-            //command is done
-            else if (splitter1[0].equals("done")){
-                int taskNumber = Integer.parseInt(splitter1[1]);
-                texts.get(taskNumber - 1).setDone();
+            catch (DukeExceptions ex){
+                System.out.println(ex.getMessage());
             }
 
-            //command is deadline
-            else if (splitter1[0].equals("deadline")){
-                String D = myString.substring(9); // after 9 letters
-                String[] splitterD = myString.split("/by ");
-                Deadline deadline = new Deadline(splitterD[0], splitterD[1]);
-                texts.add(deadline);
-                System.out.println("Got it. I've added this task:\n  " + texts.get(texts.size() - 1).toString());
-                System.out.println ("Now you have " + texts.size() + " task(s) in the list.");
-            }
-
-            //command is event
-            else if (splitter1[0].equals("event")){
-                String E = myString.substring(9); // after 6 letters
-                String[] splitterE = myString.split("/at ");
-                Event event = new Event(splitterE[0], splitterE[1]);
-                texts.add(event);
-                System.out.println("Got it. I've added this task:\n  " + texts.get(texts.size() - 1).toString());
-                System.out.println ("Now you have " + texts.size() + " task(s) in the list.");
-            }
-
-            //command is todo
-            else if (splitter1[0].equals("todo")){
-                String tempT = myString.substring(5);
-                ToDos todo = new ToDos(tempT);
-                texts.add(todo);
-                System.out.println("Got it. I've added this task:\n  " + texts.get(texts.size() - 1).toString());
-                System.out.println ("Now you have " + texts.size() + " task(s) in the list.");
-            }
             myString = scanner.nextLine();
         }
         //command is bye
